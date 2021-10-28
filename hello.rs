1d809ca996c06bfeddaca95a91e24bd4f2ef30d5
alon <alonperez2012@gmail.com>
2021-10-27 18:54:52 -0700

    demo

--- hello.rs
+++ hello.rs
@@ -8,7 +8,7 @@
 // This is the main function
 fn main() {
     // Statements here are executed when the compiled binary is called
-
+// dasdsadsada
     // Print text to the console
     println!(":)");
 }
